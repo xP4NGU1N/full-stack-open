c45d86a51864338e0a5e5ed9be336712d92c36a5
Ryan Pan <ryan123pan@gmail.com>
2024-06-22 21:25:36 +0800

    1.1-1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,14 +1,52 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.part} {props.exercise}</p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.parts[0]} exercise={props.exercises[0]} />
+      <Part part={props.parts[1]} exercise={props.exercises[1]} />
+      <Part part={props.parts[2]} exercise={props.exercises[2]} />
+    </>
+  )
+}
+
+const Footer = (props) => {
+  var total = 0
+  for (var i=0; i<props.exercises.length; i++) {
+    total += props.exercises[i]
+  }
+
+  return (
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  )
+}
+
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
-  
+  const course = 'Half Stack application development'
+  const parts = ['Fundamentals of React', 'Using props to pass data', 'State of a component']
+  const exercises = [10, 7, 14]
 
   return (
     <div>
-      <p>Hello world, it is now {now.toString()}</p>
-      <p>{a} plus {b} is {a+b}</p>
+      <Header course={course} />
+      <Content parts={parts} exercises={exercises} />
+      <Footer exercises={exercises} />
     </div>
   )
 }
